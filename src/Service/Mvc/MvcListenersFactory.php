6dcba575ae0c6cc71b676b0d8b31aaa73d6ced07
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-04-06 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/Mvc/MvcListenersFactory.php
+++ src/Service/Mvc/MvcListenersFactory.php
@@ -3,7 +3,7 @@
 namespace BotChallenge\Service\Mvc;
 
 use BotChallenge\Mvc\MvcListeners;
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class MvcListenersFactory implements FactoryInterface
